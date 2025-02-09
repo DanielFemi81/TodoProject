8188dce3c130313fc18748ca6b14f0750a887fa5
Daniel <afolayandaniel8@gmail.com>
2025-02-09 12:05:27 +0100

    updated header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div style={{ marginBottom: "20px", fontWeight: "bold" }}>
+    <div
+      style={{ marginBottom: "20px", fontWeight: "bold", textAlign: "center" }}
+    >
       MY TODO LIST FOR THE YEAR
     </div>
   );
